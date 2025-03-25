39e28187d776a710dfc1f939bafae8c3a494ed01
HOMDOUM-Janvier <janvierboreilhomdoum@gmail.com>
2025-03-26 00:54:33 +0100

    first commit

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  // Couleurs primaires
+  static const primary = Color(0xFF0033A0); // Bleu Airtel
+  static const primaryDark = Color(0xFF002366);
+
+  // Sécurité
+  static const danger = Color(0xFFFF4D4D);
+  static const safe = Color(0xFF4CD964);
+
+  // Arrière-plans
+  static const background = Color(0xFFF8F9FA);
+  static const cardBackground = Colors.white;
+}
